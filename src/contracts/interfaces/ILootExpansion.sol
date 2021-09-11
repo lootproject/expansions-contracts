b10cc897bb211848e52b8fad351a248b77da0dc9
PhABC <ph.castonguay@gmail.com>
2021-09-10 20:56:19 -0400

    Add maxSupply() to ILootExpansion

--- src/contracts/interfaces/ILootExpansion.sol
+++ src/contracts/interfaces/ILootExpansion.sol
@@ -9,4 +9,7 @@ interface ILootExpansion {
 
   // Returns the address to which the fee should be sent to
   function claimFeeRecipient() external view returns (address feeRecipient);
+
+  // Return the maximum supply for this expansion. If none, return 2**256-1
+  function maxSupply() external view returns (uint256 expansionMaxSupply);
 }
